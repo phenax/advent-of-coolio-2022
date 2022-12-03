8432aa60b55a5007d7de1dcbba2a7c34e96dcec5
Akshay Nair <phenax5@gmail.com>
2022-12-03 18:50:14 +0530

    feat: day 3 part 1 is gooood

--- day_3/js/index.js
+++ day_3/js/index.js
@@ -1,4 +1,17 @@
 const mod = require('../index.node')
+const { promises: fs } = require('fs')
 
-console.log(mod.hello())
+const loadInput = async () => {
+  const contents = await fs.readFile('./input.txt', 'utf8')
+  return processInput(contents.trim())
+}
+
+const processInput = (contents) =>
+  contents.split('\n')
+    .map(line => [
+      line.slice(0, line.length / 2),
+      line.slice(line.length / 2)
+    ])
+
+loadInput().then(mod.solve).then(console.log)
 
--- day_3/rust/lib.rs
+++ day_3/rust/lib.rs
@@ -1,11 +1,41 @@
 use neon::prelude::*;
+use std::collections::HashSet;
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
+fn get_priority(c: char) -> u8 {
+    // The worst solution
+    // (c as u8 + 20) % 58
+    match c {
+        'a'..='z' => c as u8 - 97 + 1,
+        'A'..='Z' => c as u8 - 65 + 27,
+        _ => panic!(),
+    }
+}
+
+fn solve(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    let input = cx.argument::<JsArray>(0)?;
+    let list = input.to_vec(&mut cx)?;
+
+    let mut result: i32 = 0;
+    for item in list {
+        let tuple = item.downcast_or_throw::<JsArray, _>(&mut cx)?;
+        let left = tuple.get::<JsString, _, _>(&mut cx, 0)?;
+        let right = tuple.get::<JsString, _, _>(&mut cx, 1)?;
+
+        let set_left = left.value(&mut cx).chars().collect::<HashSet<_>>();
+        let set_right = right.value(&mut cx).chars().collect::<HashSet<_>>();
+
+        let commoners = set_left.intersection(&set_right);
+        for &c in commoners {
+            let priority = get_priority(c);
+            result += priority as i32;
+        }
+    }
+
+    Ok(cx.number(result))
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("hello", hello)?;
+    cx.export_function("solve", solve)?;
     Ok(())
 }
